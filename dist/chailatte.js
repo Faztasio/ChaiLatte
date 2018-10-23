a285ecf403b04b7586b2ac494af4ab918e178715
Zack <zack@fbts.ca>
2018-10-23 17:42:55 -0600

    Create chailatte.js

---
+++ dist/chailatte.js
@@ -0,0 +1,29 @@
+define([
+  "./object"
+], function(object) {
+  
+  cl = function(means) {
+    return cl.handle(means)
+  }
+  
+  objectId = function(id) {
+    
+  }
+  
+  tag = function(obj) {
+    tag.handle(obj)
+  }
+  
+  cl.prototype = {
+  
+    handle: function(means) {
+      if (means[0] == "#") {
+        
+      }
+    }
+    
+  }
+  
+  tag.prototype = {
+  }
+});
