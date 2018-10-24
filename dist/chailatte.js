4ec3e69da0f8258a1d33e165f0d1e1a054cdd8bf
Zack <zack@fbts.ca>
2018-10-23 18:08:52 -0600

    Update chailatte.js

--- dist/chailatte.js
+++ dist/chailatte.js
@@ -1,2 +1,32 @@
-const object = require("./object.js")
+// By Zack Pace @ 2018
 
+
+
+
+module.exports = class {
+  var name = NULL
+  var data = {}
+  constructor(str) {
+    name = str
+  }
+  
+  function set(key,value) {
+    data[key] = value
+  };
+
+  function get(key,value) {
+    if (value) {
+      if (data[key] == value) {
+        return data[key]
+      } else {
+        return false
+      }
+    } else {
+      return data[key]
+    }
+  }
+
+  function getAll() {
+    return data
+  }
+};
